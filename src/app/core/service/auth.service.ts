34af162a690240e48cd5917f29765ea0d6096f5b
mustapha <mustaphaamoutaki@gmail.com>
2026-01-10 16:27:48 +0100

    feat: saving data comes form response

--- src/app/core/service/auth.service.ts
+++ src/app/core/service/auth.service.ts
@@ -1,13 +1,28 @@
 import { Injectable, inject } from "@angular/core";
 import { AuthApi } from "../../api/auth.api";
 import { LoginRequest } from "../models/auth.model";
+import { tap } from "rxjs/operators";
+
+
 @Injectable({ providedIn : 'root'})
 
 export class AuthService{
   
   private loginUrl = inject(AuthApi);
   login(data:LoginRequest){
-    return this.loginUrl.login(data); 
+    return this.loginUrl.login(data)
+     .pipe(
+      tap(res => {
+        // save tokens and role in localStorage
+        localStorage.setItem('accessToken', res.accessToken);
+
+        // save refresh token in localStorage
+        localStorage.setItem('refreshToken', res.refreshToken);
+
+        // save role in localStorage
+        localStorage.setItem('role', res.role);
+      })
+    )
   }
   
 }
