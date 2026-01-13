25f028caf51748d4b280c8ecf56afb8eec5d371e
mustapha <mustaphaamoutaki@gmail.com>
2026-01-13 10:50:21 +0100

    feat: complete supplier component route

---
+++ src/app/features/supplier/supplier.routes.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { SupplierComponent } from "./supplier.component";
+
+export const SUPPLIER_ROUTES = [
+    {
+        path: '',
+        Component: SupplierComponent
+    }
+]
